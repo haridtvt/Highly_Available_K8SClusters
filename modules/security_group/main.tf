fc6f7a95f3239ba60d4d306421eddeef5c8aef49
haridtvt <tranvanhaidtvt@gmail.com>
2026-04-08 16:18:32 +0700

    Update 6443 and DNS rules

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,15 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.99.1"
-    }
-  }
-}
 resource "aws_security_group" "SG1" {
   vpc_id = var.vpc_id
   tags = {
-    name = "All rules for master_node"
+    Name = "master_node_sg"
     terraform = "true"
   }
 }
@@ -17,9 +9,76 @@ resource "aws_security_group" "SG1" {
 resource "aws_security_group" "SG2" {
   vpc_id = var.vpc_id
   tags = {
-    name = "All rules for worker_node"
+    Name = "worker_node_sg"
     terraform = "true"
   }
+}
+
+locals {
+  target_sg_ids = {
+    master = aws_security_group.SG1.id
+    worker = aws_security_group.SG2.id
+  }
+}
+
+resource "aws_security_group_rule" "ec2_egress_internet" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  for_each = local.target_sg_ids
+  security_group_id = each.value
+}
+
+resource "aws_vpc_security_group_ingress_rule" "P22" {
+  ip_protocol       = "tcp"
+  for_each = local.target_sg_ids
+  security_group_id = each.value
+  from_port         = 22
+  to_port           = 22
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "SSH port"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "P4789" {
+  ip_protocol       = "udp"
+  for_each = local.target_sg_ids
+  security_group_id = each.value
+  from_port         = 4789
+  to_port           = 4789
+  cidr_ipv4         = var.cidr_ipv4
+  description       = "Allow VXLAN traffic"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ICMP" {
+  ip_protocol       = "icmp"
+  for_each = local.target_sg_ids
+  security_group_id = each.value
+  cidr_ipv4         = var.cidr_ipv4
+  from_port   = -1
+  to_port     = -1
+  description       = "Allow ICMP traffic"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "DNST" {
+  ip_protocol       = "tcp"
+  for_each = local.target_sg_ids
+  security_group_id = each.value
+  cidr_ipv4         = var.cidr_ipv4
+  from_port   = 53
+  to_port     = 53
+  description       = "Allow DNS TCP traffic"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "DNSU" {
+  ip_protocol       = "udp"
+  for_each = local.target_sg_ids
+  security_group_id = each.value
+  cidr_ipv4         = var.cidr_ipv4
+  from_port   = 53
+  to_port     = 53
+  description       = "Allow DNS UDP traffic"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "P2379" {
@@ -36,7 +95,7 @@ resource "aws_vpc_security_group_ingress_rule" "P6443" {
   security_group_id = aws_security_group.SG1.id
   from_port = 6443
   to_port = 6443
-  cidr_ipv4 = var.cidr_ipv4
+  cidr_ipv4 = "0.0.0.0/0"
   description = "allow 6443 port k8s API servers"
 }
 resource "aws_vpc_security_group_ingress_rule" "P10250" {
@@ -98,34 +157,6 @@ resource "aws_vpc_security_group_ingress_rule" "P30000U" {
   security_group_id = aws_security_group.SG2.id
   from_port         = 30000
   to_port           = 32767
-  cidr_ipv4         = var.cidr_ipv4
+  cidr_ipv4         = "0.0.0.0/0"
   description       = "NodePort Services "
 }
-
-locals {
-  target_sg_ids = {
-    master = aws_security_group.SG1.id
-    worker = aws_security_group.SG2.id
-  }
-}
-
-resource "aws_security_group_rule" "ec2_egress_internet" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  for_each = local.target_sg_ids
-  security_group_id = each.value
-}
-
-resource "aws_vpc_security_group_ingress_rule" "P22" {
-  ip_protocol       = "udp"
-  for_each = local.target_sg_ids
-  security_group_id = each.value
-  from_port         = 22
-  to_port           = 22
-  cidr_ipv4         = var.cidr_ipv4
-  description       = "SSH port"
-}
-
