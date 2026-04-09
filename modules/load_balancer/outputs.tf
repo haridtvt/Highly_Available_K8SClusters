718f9fbc11b5bcc24525fb88f63d67c39ec8cd9c
hari <tranvanhaidtvt@gmail.com>
2026-04-09 23:07:35 +0700

    Update output dns lb

--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -1,3 +1,16 @@
 output "target_group_alb_worker_arn" {
   value = aws_lb_target_group.application_lb_target.arn
 }
+
+output "nlb_external_dns" {
+  value = aws_lb.nbl_external.dns_name
+}
+
+output "nlb_internal_dns" {
+  value = aws_lb.nbl_internal.dns_name
+}
+
+output "alb_external_dns" {
+  value = aws_lb.application_lb.dns_name
+}
+
