7552ee5338e1f8ec5324b66eb3f64ca8edf37c4f
hari <tranvanhaidtvt@gmail.com>
2026-04-09 21:38:00 +0700

    Adding output for subnets id

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -7,13 +7,37 @@ output "vpc_cidr" {
 }
 
 output "subnet_private_zone1a" {
-  value = aws_subnet.subnet_public_1a.cidr_block
+  value = aws_subnet.subnet_private_1a.cidr_block
 }
 
 output "subnet_private_zone1b" {
-  value = aws_subnet.subnet_public_1b.cidr_block
+  value = aws_subnet.subnet_private_1b.cidr_block
 }
 
 output "subnet_private_zone1c" {
-  value = aws_subnet.subnet_public_1c.cidr_block
+  value = aws_subnet.subnet_private_1c.cidr_block
+}
+
+output "subnet_private_zone1a_id" {
+  value = aws_subnet.subnet_private_1a.id
+}
+
+output "subnet_private_zone1b_id" {
+  value = aws_subnet.subnet_private_1b.id
+}
+
+output "subnet_private_zone1c_id" {
+  value = aws_subnet.subnet_private_1c.id
+}
+
+output "subnet_public_zone1a_id" {
+  value = aws_subnet.subnet_public_1a.id
+}
+
+output "subnet_public_zone1b_id" {
+  value = aws_subnet.subnet_public_1b.id
+}
+
+output "subnet_public_zone1c_id" {
+  value = aws_subnet.subnet_public_1c.id
 }
