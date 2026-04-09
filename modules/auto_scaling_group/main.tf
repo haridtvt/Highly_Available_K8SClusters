ac6bd93d04e189b73ab0742a9d7ad55a50e2cc37
hari <tranvanhaidtvt@gmail.com>
2026-04-09 21:34:56 +0700

    Implement for asg worker nodes

---
+++ modules/auto_scaling_group/main.tf
@@ -0,0 +1,20 @@
+resource "aws_autoscaling_group" "worker_asg" {
+  name                = "worker-asg"
+  desired_capacity    = 3
+  max_size            = 6
+  min_size            = 3
+  vpc_zone_identifier = var.public_subnet_ids
+
+  launch_template {
+    id = var.template_worker
+    version = "$Latest"
+  }
+  target_group_arns = [var.target_group_arn]
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+  tag {
+    key                 = "true"
+    propagate_at_launch = false
+    value               = "terraform"
+  }
+}
---
+++ modules/auto_scaling_group/variables.tf
@@ -0,0 +1,3 @@
+variable "public_subnet_ids" {}
+variable "template_worker" {}
+variable "target_group_arn" {}
